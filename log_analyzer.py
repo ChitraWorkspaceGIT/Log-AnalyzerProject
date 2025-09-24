f74aefa7ee4fcff5f475cf4962a205747b75c2ce
Chinthala Chitra <chinthalachitra28@gmail.com>
2025-09-24 14:43:15 +0530

    first commit

---
+++ log_analyzer.py
@@ -0,0 +1,26 @@
+# log_analyzer.py
+
+from collections import Counter
+
+# Open the log file
+with open("nginx_access.log", "r") as f:
+    logs = f.readlines()
+
+
+# 1. Count 404 errors
+errors_404 = [line for line in logs if " 404 " in line]
+print(f"Total 404 errors: {len(errors_404)}")
+
+# 2. Most requested pages
+pages = [line.split('"')[1].split()[1] for line in logs if '"' in line]
+top_pages = Counter(pages).most_common(5)
+print("\nTop requested pages:")
+for page, count in top_pages:
+    print(f"{page} - {count} times")
+
+# 3. Most active IP addresses
+ips = [line.split()[0] for line in logs]
+top_ips = Counter(ips).most_common(5)
+print("\nTop IP addresses:")
+for ip, count in top_ips:
+    print(f"{ip} - {count} times")
